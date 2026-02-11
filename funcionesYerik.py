adeeb112b0ad06fff7f6796c4a7a956b8b34d562
YerikRdgz <yerikaxel00@gmail.com>
2026-02-10 23:22:34 -0600

    Implementacion de bucle while en archivo main

--- funcionesYerik.py
+++ funcionesYerik.py
@@ -1,8 +1,3 @@
-import baseDatos as bd
-
-
-def consultar_info(nombre):
-    info = bd.df_pedidos.loc[
-        lambda i: i["cliente"].str.contains(nombre, case=False, na=False)
-    ]
+def consultar_info(df, nombre):
+    info = df.loc[lambda i: i["cliente"].str.contains(nombre, case=False, na=False)]
     return info
--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 import baseDatos as bd
+import funcionesPuras as n
 import funcionesYerik as y
 
 bd.df_pedidos.to_csv("pedidos.csv", index=False)
+df = bd.df_pedidos
+while True:
+    print("=" * 50)
+    print("OPCIONES")
+    print("=" * 50)
+    print("""
+    1. Calcular totales de pedidos
+    2. Identificar pedidos pagados
+    3. Consultar información por cliente
+    4. Inferir información a partir de reglas lógicas
+    5. Salir
+    """)
 
-print("=" * 50)
-print("OPCIONES")
-print("=" * 50)
-print("""
-1. Calcular totales de pedidos
-2. Identificar pedidos pagados
-3. Consultar información por cliente
-4. Inferir información a partir de reglas lógicas
-0. Salir
-""")
-
-opcion = str(input("Elige la opción: "))
-match opcion:
-    case "3":
-        nombre = str(input("Ingrese el nombre del cliente: "))
-        print(y.consultar_info(nombre))
-    case _:
-        print("Opcion invalida")
+    opcion = str(input("Elige la opción: "))
+    match opcion:
+        case "1":
+            total_pedidos = n.total_pedidos(df)
+            print(f"El numero total de pedidos es: {total_pedidos}")
+        case "2":
+            total_pagados = n.total_pagados(df)
+            print(total_pagados)
+        case "3":
+            nombre = str(input("Ingrese el nombre del cliente: "))
+            print(y.consultar_info(df, nombre))
+        case "0":
+            print("Saliendo...")
+            break
+        case _:
+            print("Opcion invalida")
