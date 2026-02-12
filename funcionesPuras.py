8121d02753c2cf3e6cef33cb2e43374850b291ec
YerikRdgz <yerikaxel00@gmail.com>
2026-02-12 14:58:40 -0600

    Neil y Schos

--- funcionesPuras.py
+++ funcionesPuras.py
@@ -2,23 +2,6 @@ def total_pedidos(df):
     return len(df)
 
 
-"""
 def pedidos_pagados(x):
-    df_pagados=x[x['estado']== 'pagado']
+    df_pagados = x[x["estado"] == "pagado"]
     return len(df_pagados)
-"""
-
-
-def total_pagados(df):
-    if df.empty:
-        return 0
-
-    cabeza = df.iloc[0]
-    cola = df.iloc[1:]
-
-    if cabeza["estado"] == "pagado":
-        valor_actual = cabeza["cantidad"] * cabeza["precio"]
-    else:
-        valor_actual = 0
-
-    return valor_actual + total_pagados(cola)
--- main.py
+++ main.py
@@ -12,8 +12,9 @@ while True:
     1. Calcular totales de pedidos
     2. Identificar pedidos pagados
     3. Consultar información por cliente
-    4. Inferir información a partir de reglas lógicas
-    5. Salir
+    4. Pedidos listos para envio
+    5. Ingreso total de los pedidos pagados
+    0. Salir
     """)
 
     opcion = str(input("Elige la opción: "))
@@ -24,9 +25,9 @@ while True:
             print(f"El numero total de pedidos es: {total_pedidos}")
             print("=" * 50, "\n")
         case "2":
-            total_pagados = n.total_pagados(df)
+            pedidos_pagados = n.pedidos_pagados(df)
             print("=" * 50)
-            print(f"El ingreso total de los pedidos pagados es: {total_pagados}")
+            print(f"El numero total de pedidos pagados es: {pedidos_pagados}")
             print("=" * 50, "\n")
         case "3":
             nombre = str(input("\nIngrese el nombre del cliente: "))
@@ -38,6 +39,11 @@ while True:
             print("\nPedidos listos para envio\n")
             print("=" * 50)
             print(y.inferir_info(df))
+            print("=" * 50, "\n")
+        case "5":
+            total_pagados = n.total_pagados(df)
+            print("=" * 50)
+            print(f"El ingreso total de los pedidos pagados es: {total_pagados}")
             print("=" * 50, "\n")
         case "0":
             print("\nSaliendo...\n")
